1682f2682e872bb886c0357b1019047219896efd
vaibhav <59561631+v4ibhava@users.noreply.github.com>
2026-03-27 17:24:51 +0530

    small changes in CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: FRONTEND_URL,
+    origin: [FRONTEND_URL],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
